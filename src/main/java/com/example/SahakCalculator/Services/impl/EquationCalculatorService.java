73d48b7baf1820158f4b24e22712070438b6082b
sahar <shrsahebi@gmail.com>
2020-10-28 16:56:40 +0330

    services

--- src/main/java/com/example/SahakCalculator/Services/impl/EquationCalculatorService.java
+++ src/main/java/com/example/SahakCalculator/Services/impl/EquationCalculatorService.java
@@ -4,6 +4,7 @@ import com.example.SahakCalculator.Model.QuestionModel;
 import com.example.SahakCalculator.Services.CalculatorService;
 
 public class EquationCalculatorService implements CalculatorService {
+    //firstservice
     @Override
     public QuestionModel calculate(String question) {
         return null;
