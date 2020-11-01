8bde68b6724926e2ffc9d92e943c95e857cc3c6f
sahar <shrsahebi@gmail.com>
2020-11-01 14:36:41 +0330

    error handling added

--- src/main/java/com/example/SahakCalculator/bean/Converter.java
+++ src/main/java/com/example/SahakCalculator/bean/Converter.java
@@ -1,18 +1,5 @@
 package com.example.SahakCalculator.bean;
 
 public class Converter {
-    static int precedence(char c){
-        switch (c){
-            case '+':
-            case '-':
-                return 1;
-            case '*':
-            case '/':
-                return 2;
-            case '^':
-                return 3;
-        }
-        return -1;
-    }
-
+//in progress
 }
--- src/main/java/com/example/SahakCalculator/bean/EquationsCalculator.java
+++ src/main/java/com/example/SahakCalculator/bean/EquationsCalculator.java
@@ -1,5 +1,5 @@
 package com.example.SahakCalculator.bean;
 
 public class EquationsCalculator {
-
+//in progress
 }
